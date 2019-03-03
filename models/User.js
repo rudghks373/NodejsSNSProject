b5b001b1cc91f5785233f2e123ef6a53849b79b4
rudghks373 <rudghks373@naver.com>
2019-03-03 19:11:08 +0900

    Updata password bcryp hash

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 // models/User.js
 
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
 
 // schema // 1
 var userSchema = mongoose.Schema({
@@ -44,18 +45,34 @@ userSchema.path("password").validate(function(v) {
    
     // update user // 3-4
     if(!user.isNew){
-     if(!user.currentPassword){
-      user.invalidate("currentPassword", "Current Password is required!");
-     }
-     if(user.currentPassword && user.currentPassword != user.originalPassword){
-      user.invalidate("currentPassword", "Current Password is invalid!");
-     }
-     if(user.newPassword !== user.passwordConfirmation) {
-      user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
-     }
-    }
-   });
-   
+        if(!user.currentPassword){
+         user.invalidate("currentPassword", "Current Password is required!");
+        }
+        if(user.currentPassword && !bcrypt.compareSync(user.currentPassword, user.originalPassword)){ // 2
+         user.invalidate("currentPassword", "Current Password is invalid!");
+        }
+        if(user.newPassword !== user.passwordConfirmation) {
+         user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
+        }
+       }
+      });
+
+      userSchema.pre("save", function (next){
+        var user = this;
+        if(!user.isModified("password")){ // 3-1
+         return next();
+        } else {
+         user.password = bcrypt.hashSync(user.password); // 3-2
+         return next();
+        }
+       });
+       
+       userSchema.methods.authenticate = function (password) {
+        var user = this;
+        return bcrypt.compareSync(password,user.password);
+       };
+
+
    // model & export
    var User = mongoose.model("user",userSchema);
    module.exports = User;
